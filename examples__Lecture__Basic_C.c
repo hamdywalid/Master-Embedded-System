8bcb86c50862af7104c1a1526dd3a0ba993567a7
hamdywalid <130710021+hamdywalid@users.noreply.github.com>
2023-10-20 05:04:56 +0300

    Unit2__C__Programming

---
+++ examples__Lecture__Basic_C.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() 
+{ 
+//bool a = true; 
+//return 0; 
+
+
+//int x=5;
+//float y=2.0;
+//if(x/y ==2.5){
+//    printf("int/float =float");}
+//else if(x/y ==2){
+//    printf("int/float =int");}
+
+
+//printf("%f\n",5/2);
+//printf("%f\n",5.0/2);
+//printf("%f\n",5/2.0);
+//printf("%f\n",5.0/2.0);
+//printf("\n");
+//printf("%f\n",2/5);
+//printf("%f\n",2.0/5);
+//printf("%f\n",2/5.0);
+//printf("%f\n",2.0/5.0);
+
+
+// float a, b, c ;
+// int s;
+// s = a * b * c / 100 + 32 / 4 - 3 * 1.1;
+// printf("s=%d",s);
+
+
+// int k;
+// printf("%d\n",k=9/2);
+// printf("%d\n",k=9/2.0);
+// printf("%d\n",k=9.0/2);
+// printf("%d\n",k=9.0/2.0);
+// printf("%d\n",k=2/9);
+// printf("%d\n",k=2/9.0);
+// printf("%d\n",k=2.0/9);
+// printf("%d\n",k=2.0/9.0);
+// printf("\n");
+// float a;
+// printf("%f\n",a=9/2);
+// printf("%f\n",a=9/2.0);
+// printf("%f\n",a=9.0/2);
+// printf("%f\n",a=9.0/2.0);
+// printf("%f\n",a=2/9);
+// printf("%f\n",a=2/9.0);
+// printf("%f\n",a=2.0/9);
+// printf("%f\n",a=2.0/9.0);
+
+
+// int  x=10;
+// char y='b';
+// float z=x+y;
+// printf("%d %f\n",x,z);
+
+
+// double x=10.45;
+// int sum=(int)x+1;
+// printf("%d\n",sum);
+
+
+// int integer;
+// printf("enter the number:");
+// fflush(stdin);fflush(stdout);
+// scanf("%d",&integer);
+// printf("integer:%d\n",integer);
+
+
+// float float_num;
+// printf("enter the float_number:");
+// fflush(stdin);fflush(stdout);
+// scanf("%f",&float_num);
+// printf("float_num:%f\n",float_num);
+
+
+// int a = 9;
+// int b = 8;
+// int c = 12;
+// printf("%d\n", a>b);
+// printf("%d\n", b>c);
+// printf("%d\n", a<=9);
+// printf("%d\n", a!=9);
+// printf("%d\n", (a-b)>(c-b));
+// printf("%d\n", a>b && c>b);
+// printf("%d\n", a>b && c<b);
+// printf("%d\n", a>b || c<b);
+// printf("!(a<b):: %d\n", !(a<b));
+// printf("3 && 0:: %d\n", 3 && 0);
+// printf("-15 || 0:: %d\n", -15 || 0);
+// printf("!(-15):: %d\n", !(-15));
+// printf("!(0):: %d\n", !(0));
+
+
+
+// 	char choice;
+// 	float radius,area,circumference;
+
+// 	printf("enter circle radius:");
+// 	scanf("%f",&radius);
+// 	printf("enter your choice area or circumference ");
+// 	scanf("%c",&choice);
+
+// 	switch(choice)
+// 	{
+// 	 case'A':
+// 	 case'a':
+// 	 {
+// 	 float radius;  
+// 	 float area=3.14 * radius * radius;
+// 	 printf("\r\n area %f \r\n",area);
+// 	 }
+// 	 break;
+// 	 case'C':
+// 	 case'c':
+// 	 {
+// 	 float radius;
+// 	 float circumference=2*3.14*radius;
+//      printf("\r\n circumference %f \r\n", circumference);
+// 	 }
+// 	 break;
+// 	 default:
+// 	 printf("\r\n wrong choice \r\n");
+//      break;
+// 	}
+
+
+
+// int i;
+// for(i=0;i<=100;++i){
+//     printf("%d : Hello World\n",i);
+// }
+
+//int   i=1;
+//int sum=0;
+//while(i<=99)
+//{
+//    sum+=i;
+//    i+=2;
+//}
+// printf("%d",sum);
+
+
+}
