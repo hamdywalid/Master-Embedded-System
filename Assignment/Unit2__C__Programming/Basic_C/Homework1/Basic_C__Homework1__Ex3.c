9f95e25f979e9e0fd77dc33716925b2fea624663
hamdywalid <130710021+hamdywalid@users.noreply.github.com>
2023-10-20 04:52:58 +0300

    Unit2__C__Programming

---
+++ Assignment/Unit2__C__Programming/Basic_C/Homework1/Basic_C__Homework1__Ex3.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 28, 2023
+ *      Author: DELL
+ */
+
+//  EX1:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main (void) {
+	int x,y;
+	printf("Enter two integers:\n");
+	fflush(stdin);fflush(stdout);
+	scanf("%d %d",&x,&y);
+	printf("\n Sum :%d",x+y);
+	return 0;
+}
+
+
+
+
+
+
